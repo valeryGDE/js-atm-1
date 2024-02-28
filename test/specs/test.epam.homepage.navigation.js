d28ce3fedb6ca11f4057ef1a084d664d781b310c
“Valeryia <valeryia_kazlova@epam.com>
2024-02-28 19:31:17 +0100

    intro task

---
+++ test/specs/test.epam.homepage.navigation.js
@@ -0,0 +1,40 @@
+import { expect, assert } from "chai";
+
+describe('WebdriverIO Introduction', () => {
+    before(async () => {
+        await browser.url('/');
+        const acceptCookies = await $("button[id*='onetrust-accept']");
+        await acceptCookies.click();
+    });
+
+    it('navigation to services tab', async () => {
+        const servicesTab = await $("span>a[href='/services']");
+        await servicesTab.click();
+        const title = await browser.getTitle()
+        expect(title).to.be.equal('Services | EPAM')
+    });
+
+    it('navigation to insights tab', async () => {
+        const insightsTab = await $('//span/a[text()="Insights"]');
+        await insightsTab.click();
+        const title = await browser.getTitle()
+        assert.equal(title, 'Discover our Latest Insights | EPAM');
+    });
+
+    it('color changes on hovering tab', async () => {
+        const aboutTab = await $("span>a[href='/about']");
+        const colorBeforeHover = await aboutTab.getCSSProperty('color');
+        await aboutTab.moveTo();
+        await browser.pause(500);
+        const colorAfterHover = await aboutTab.getCSSProperty('color');
+        console.log(colorAfterHover.parsed.hex)
+        expect(colorBeforeHover.parsed.hex).not.to.equal(colorAfterHover.parsed.hex);
+    });
+
+    it('navigation to main page via logo', async () => {
+        const logo = await $("a[class*= 'desktop-logo']");
+        await logo.click();
+        const title = await browser.getTitle()
+        assert.equal(title, 'EPAM | Software Engineering & Product Development Services');
+    });
+});
